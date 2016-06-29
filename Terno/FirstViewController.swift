a80d4d16fe36dcecf1c07df1b057f29bf3a01995
Kylar13 <ramonpans13@gmail.com>
2016-06-29 10:09:55 -0700

    Initial Commit

---
+++ Terno/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Terno
+//
+//  Created by Ramon Pans on 29/06/16.
+//  Copyright © 2016 Flatmates. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
